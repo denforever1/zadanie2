10b243b7b6fd8efd798c1e444f74d61b0df9e929
denforever1 <daniilkartavyh18740p@gmail.com>
2026-06-23 11:03:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace zadanie2 
+{
+    public class ArrayHelper
+    {
+        public int[] TakeFirstN(int[] numbers, int n)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException(nameof(numbers));
+
+            if (n < 0 || n > numbers.Length)
+                throw new ArgumentOutOfRangeException(nameof(n));
+
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = numbers[i];
+            }
+
+            return result;
+        }
+    }
+}
